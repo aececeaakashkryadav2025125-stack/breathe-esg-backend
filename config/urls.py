43d05de74077e692d8a2e9dc1b0d48f6b17d649b
Aakash Kumar Yadav <aec.ece.aakashkryadav.2025.125@gmail.com>
2026-05-26 17:13:08 +0530

    Fixed homepage route

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+
+def home(request):
+    return JsonResponse({
+        "message": "Breathe ESG Backend Running",
+        "admin": "/admin/",
+        "api_records": "/api/records/",
+        "api_upload": "/api/upload/"
+    })
+
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('ingestion.urls')),
 ]
